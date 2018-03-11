0736f1806de519ccbb50921f9bd13909ef67f62e
JD Bothma <jbothma@gmail.com>
2018-03-11 13:58:54 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sanews',
+    version='0.0.1',
+    description='Database of South African news articles',
+    url='https://github.com/public-people',
+    author='JD Bothma',
+    author_email='jbothma@gmail.com',
+    license='MIT',
+    packages=["sanews"],
+    zip_safe=False,
+)
