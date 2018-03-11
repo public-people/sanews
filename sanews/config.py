14aba564e5a122c983329a17b3f91cd8312efd15
JD Bothma <jbothma@gmail.com>
2018-03-11 14:26:36 +0200

    Fix config

--- sanews/config.py
+++ sanews/config.py
@@ -1,1 +1,4 @@
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://sanews@localhost/sanews')
+import os
+
+
+DATABASE_URL = os.environ.get('SANEWS_DATABASE_URL', 'postgresql://sanews@localhost/sanews')
